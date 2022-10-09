6a06c306f7c58d2962f9f4000164c3a541d1962b
xuliji <2384465976@qq.com>
2022-10-09 18:41:47 +0800

    update

--- 问题1_分区画图.py
+++ 问题1_分区画图.py
@@ -10,25 +10,25 @@ plt.rcParams["font.sans-serif"]='KaiTi'   #解决中文乱码问题
 plt.rcParams['axes.unicode_minus']=False   #解决负号无法显示的问题
 
 df_1 = pd.read_excel('./附件1：长春市COVID-19疫情期间病毒感染人数数据.xlsx', sheet_name=0, header=0, usecols=[0]+list(range(10, 27)))
-df_3 = df_1.iloc[:, 1:]
+df_3 = df_1.iloc[:, 2:]
 print(df_3)
 
 df_2 = pd.read_excel('./附件1：长春市COVID-19疫情期间病毒感染人数数据.xlsx', sheet_name=1, header=0)
-df_4 = df_2.iloc[:, 1:]
+df_4 = df_2.iloc[:, 2:]
 
 
 df_sum = df_3+df_4
 df_time = df_1.iloc[:, 0]
 data = pd.concat([df_time,df_sum],axis=1).values
 # data.to_excel('./sum_data.xlsx')
-location = ['全市总计','九台区','长春新区','净月区','绿园区','朝阳区','经开区','双阳区','宽城区','南关区','汽开区','二道区','莲花山区','公主岭市','德惠市','榆树市','农安县']
+location = ['九台区','长春新区','净月区','绿园区','朝阳区','经开区','双阳区','宽城区','南关区','汽开区','二道区','莲花山区','公主岭市','德惠市','榆树市','农安县']
 
 
 plt.figure(figsize=(12, 6))
 plt.title('长春市疫情发展总体趋势图', fontsize=16)
 plt.xlabel("日期", fontsize=16)
 plt.ylabel("总感染人数", fontsize=16)
-markers = ['-*', '-o', '-^', '-+', '-x', '-+', '-o', '-^', '-p', '-d']
+markers = ['-*', '-o', '-^', '-+', '-x', '-+', '-v', '-^', '-p', '-d']
 for i in range(1, data.shape[1]):
     marker = markers[i%len(markers)]
     plt.plot(data[:, 0], data[:, i], marker,label=location[i-1])
